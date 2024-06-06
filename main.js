a30b85d6f98dac964fa5b4d507c75a9156510575
julio gabriel barichello <108533419+kellylinda@users.noreply.github.com>
2024-06-06 11:00:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const ciaxaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-pergntas');
+const caixaAlternativas = document.querySelector('.caixa-altenativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
