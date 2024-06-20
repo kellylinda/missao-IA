0c75c7af06c6f01ce6da35f54b71923e49f61d66
julio gabriel barichello <108533419+kellylinda@users.noreply.github.com>
2024-06-20 11:57:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -22,10 +22,46 @@ const perguntas = [
     },
     {
         enunciado:"Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: aomo a IA empacta o trabalho do futuro. Nessa debate, como você se posiciona?",
-        altenativas: [
+        alternativas: [
             "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhor habilidade humanas.",
             "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importancia de proteger os trabalhadores."
         ]
 
     },
-]
+
+    {
+        enuciado: "Ao final da duscução, você precisou criar uma imagem no computador que representa o que pensa sobre IA. E agora?",
+        alternativas: [
+            "Criar uma imagem utilizando uma plataforma de desing como Paint.",
+            "criar uma imagem utilizando um gerador de imagem de IA.",
+        ]
+    },
+
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho esta um pouco atrazado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que está totalmente igual ao chat. O que voce faz?",
+        alternativas:[
+            "Escrever comandos para o chat é ima forma de contribuir para o trabalho, por isso não é ptoblema utilizar o texto inteiro.",
+            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda maquina erra, por isso revisar o trabalho e comtribuir com as perspectivas pessoais e essencial.",
+
+        ]
+    }
+];
+
+let atual = o;
+let perguntaAltual;
+
+function mostraPergunta(){
+    perguntaAltual = perguntas[atual]
+    caixaPerguntas.textContent=perguntaAltual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for (const alternativa of perguntaAltual.alternativas){
+        const botaoAlternativas=document.createElement("button")
+        botaoAlternativas.textContent=alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+
